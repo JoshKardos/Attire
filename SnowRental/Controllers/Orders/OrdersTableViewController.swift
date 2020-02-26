628fbcb9b101b9247e03a0ee7de4f34fa4a3b520
Josh Kardos <joshkardos@gmail.com>
2020-02-25 17:14:22 -0800

    starting orders

---
+++ SnowRental/Controllers/Orders/OrdersTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OrdersTableTableViewController.swift
+//  SnowRental
+//
+//  Created by Josh Kardos on 2/25/20.
+//  Copyright © 2020 Josh Kardos. All rights reserved.
+//
+
+import UIKit
+
+class OrdersTableViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+extension OrdersTableViewController: UITableViewDelegate, UITableViewDataSource {
+    
+}
